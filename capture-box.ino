0f8b6a66b71b206e9c624bcb45e0f751178fa3f4
Andrei Ciubotaru <andrei.ciubotaru@ness.com>
2021-10-19 15:17:31 +0300

    test

--- capture-box.ino
+++ capture-box.ino
@@ -48,33 +48,7 @@ char keys[rows][cols] = {
 byte rowPins[rows] = {45,43,41,39};
 byte colPins[cols] = {37,35,33,31};
 
-//sound constants
-const int c = 261;
-const int d = 294;
-const int e = 329;
-const int f = 349;
-const int g = 391;
-const int gS = 415;
-const int a = 440;
-const int aS = 455;
-const int b = 466;
-const int cH = 523;
-const int cSH = 554;
-const int dH = 587;
-const int dSH = 622;
-const int eH = 659;
-const int fH = 698;
-const int fSH = 740;
-const int gH = 784;
-const int gSH = 830;
-const int aH = 880;
 
-int counter = 0;
-
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
-
-// LCD
-LiquidCrystal_I2C lcd(0x3F,16,2); // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 
 
